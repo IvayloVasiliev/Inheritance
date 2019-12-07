f40761592ec488c4e4a33032a31e295daa81e9f9
Ivailo Vasiliev <ivailo_vasiliev@hotmail.com>
2019-12-07 23:58:20 +0200

    RadioDatabase

---
+++ OnlineRadioDatabase04/Exceptions/InvalidSongNameException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineRadioDatabase04.Exceptions
+{
+    public class InvalidSongNameException : InvalidSongException
+    {
+        public InvalidSongNameException(string message = "Song name should be between 3 and 30 symbols.")
+            : base(message)
+        {
+        }
+    }
+}
---
+++ OnlineRadioDatabase04/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using OnlineRadioDatabase04.Core;
+using System;
+
+namespace OnlineRadioDatabase04
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+            engine.Run();
+
+
+        }
+    }
+}
